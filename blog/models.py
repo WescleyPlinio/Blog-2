953f465363506f14216b6d71d32c4d3e0b575dee
Wescley <wescleygaldino06@gmail.com>
2024-11-01 08:01:43 -0300

    Criando model blog

--- blog/models.py
+++ blog/models.py
@@ -11,3 +11,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.titulo
+    
+class Blog(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(max_length=2000)
+    capa = models.ImageField(blank=True)
+    logo = models.ImageField()
+    instagram = models.URLField(blank=True)
+    facebook = models.URLField(blank=True)
+    github = models.URLField(blank=True)
+    autores = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.nome
