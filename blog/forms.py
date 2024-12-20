7d54ba5fca590aae4f2be83604d37d23c19db470
Wescley <wescleygaldino06@gmail.com>
2024-12-20 09:54:21 -0300

    Criando e otimizando formulários

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Mensagem, Blog
+from .models import Mensagem, Blog, Post
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Row, Column, Submit
 
@@ -21,6 +21,27 @@ class MensagemForm(forms.ModelForm):
             Row(
                 Column('email', css_class='col-md-12'),
                 Column('mensagem', css_class='col-12'),
+                css_class='row'
+            ),
+            Submit('submit', 'Enviar', css_class='btn btn-primary text-uppercase')
+        )
+        
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ["titulo", "subtitulo", "usuario", "conteudo", "capa"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            Row(
+                Column('titulo', css_class='col-md-8'),
+                Column('capa', css_class='col-md-4'),
+                Column('subtitulo', css_class='col-md-12'),
+                Column('conteudo', css_class='col-12'),
+                Column('usuario', css_class='col-12'),
                 css_class='row'
             ),
             Submit('submit', 'Enviar', css_class='btn btn-primary text-uppercase')
