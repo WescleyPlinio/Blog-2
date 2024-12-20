65c6ab3b4a7e06306361f349ad6971cef2a95e4c
Wescley <wescleygaldino06@gmail.com>
2024-12-20 08:01:35 -0300

    Otimizando Crispy form

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,32 @@
 from django import forms
-from .models import Mensagem
+from .models import Mensagem, Blog
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Row, Column, Submit
 
 class MensagemForm(forms.ModelForm):
     class Meta:
         model = Mensagem
         fields = ["nome", "email", "telefone", "mensagem"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            Row(
+                Column('nome', css_class='col-md-6'),
+                Column('telefone', css_class='col-md-6'),
+                css_class='row'
+            ),
+            Row(
+                Column('email', css_class='col-md-12'),
+                Column('mensagem', css_class='col-12'),
+                css_class='row'
+            ),
+            Submit('submit', 'Enviar', css_class='btn btn-primary text-uppercase')
+        )
         
-          
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model = Blog.objects.first()
+        fields = "__all__"
